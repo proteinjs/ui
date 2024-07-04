c1db5fef10f80c1554d0eab66ef69edcb24b0cd5
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-07-03 18:12:50 -0700

    generate default react query keys

--- packages/ui/src/table/TableLoader.ts
+++ packages/ui/src/table/TableLoader.ts
@@ -1,24 +1,38 @@
+import { v1 as uuidv1 } from 'uuid';
+
 export type RowWindow<T> = {
   rows: T[];
   totalCount: number;
 };
 
+export type ReactQueryKeys = {
+  /** Unique name of data set */
+  dataKey: string;
+  /** Unique name to differentiate between queries on the data set */
+  dataQueryKey: string;
+};
+
 export type TableLoader<T> = {
-  // TODO: make this optional
-  reactQueryKeys: {
-    /** Unique name of data set */
-    dataKey: string;
-    /** Unique name to differentiate between queries on the data set */
-    dataQueryKey: string;
-  };
+  reactQueryKeys?: ReactQueryKeys;
   load: (startIndex: number, endIndex: number) => Promise<RowWindow<T>>;
 };
+
+export function generateDefaultReactQueryKeys(): ReactQueryKeys {
+  return {
+    dataKey: uuidv1(),
+    dataQueryKey: uuidv1(),
+  };
+}
 
 export class StaticTableLoader<T> implements TableLoader<T> {
   constructor(
     private list: T[],
-    public reactQueryKeys: TableLoader<T>['reactQueryKeys']
-  ) {}
+    public reactQueryKeys?: TableLoader<T>['reactQueryKeys']
+  ) {
+    if (!this.reactQueryKeys) {
+      this.reactQueryKeys = generateDefaultReactQueryKeys();
+    }
+  }
 
   async load(startIndex: number, endIndex: number) {
     return {
